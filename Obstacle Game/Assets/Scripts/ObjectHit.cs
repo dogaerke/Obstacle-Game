1eb22a190dc5d62974f1ed1b28e2f8abbc30a2d8
Doğa Melis ERKE <dogaerke2001@gmail.com>
2023-07-26 21:38:53 +0300

    The game has finished.

--- Obstacle Game/Assets/Scripts/ObjectHit.cs
+++ Obstacle Game/Assets/Scripts/ObjectHit.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
+        if(other.gameObject.tag == "Player"){
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
 
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        }
     }
 }
